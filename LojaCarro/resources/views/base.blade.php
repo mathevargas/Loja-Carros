561d42587e52371cc32c70c229231faf4d37e4d2
Matheus Vargas <matheusvargask2@gmail.com>
2024-06-22 14:31:15 -0300

    adição de uma imagem de fundo, renomeação de alguns botões e ajuste no tamanho das faixas coloridas em css.

--- LojaCarro/resources/views/base.blade.php
+++ LojaCarro/resources/views/base.blade.php
@@ -5,30 +5,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    {{-- Pegando as variáveis de ambiente --}}
     <title>{{ env('APP_NAME') }}</title>
+    <style>
+        body {
+            background-image: url('foto_loja.jpg');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            height: 100vh;
+            margin: 0;
+        }
+    </style>
     @vite('resources/css/app.css')
 </head>
-
-<body>
-    <div class="container">
-        <header>
-            <nav class="border-gray-200 px-4 lg:px-6 py-2.5 bg-gray-800">
-                <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                        <span class="self-center text-xl font-semibold whitespace-nowraptext-white">Loja de Carros</span>
-                    <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                        <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            <li>
-                                <a href="/carros" class="block py-2 pr-4 pl-3 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 text-white" aria-current="page">Menu</a>
-                            </li>
-                            <li>
-                                <a href="/carros/create" class="block py-2 pr-4 pl-3 border-b lg:border-0 lg:hover:text-primary-700 lg:p-0 text-gray-400 lg:hover:text-white hover:bg-gray-700 over:text-white lg:hover:bg-transparent border-gray-700">Criar</a>
-                            </li>
-                        </ul>
-                    </div>
+<body class="bg-gray-100">
+    <header class="bg-gray-800">
+        <div class="container mx-auto px-4 lg:px-6 py-2.5">
+            <div class="flex justify-between items-center max-w-screen-xl">
+                <span class="text-xl font-semibold whitespace-nowrap text-white">Loja de Carros</span>
+                <div class="hidden lg:flex lg:w-auto" id="mobile-menu-2">
+                    <ul class="flex space-x-8">
+                        <li>
+                            <a href="/carros" class="block py-2 px-3 rounded bg-primary-700 text-white">Lista</a>
+                        </li>
+                        <li>
+                            <a href="/carros/create" class="block py-2 px-3 rounded bg-primary-700 text-white hover:bg-gray-700">Cadastrar</a>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
-        </header>
+            </div>
+        </div>
+    </header>
+    <div class="container mx-auto mt-4 px-4 lg:px-6"> <!-- Ajustei a margem superior para separar do cabeçalho -->
         <div class="content">
             @yield('content')
         </div>
--- LojaCarro/resources/views/carros/create.blade.php
+++ LojaCarro/resources/views/carros/create.blade.php
@@ -46,7 +46,7 @@
       </div>
     </div>
     <div class="flex">
-      <div class="w-72 ml-2">
+      <div class="w-72 ml-5">
         <div class="relative w-full min-w-[200px] h-10">
           <input required name="cor" id="cor" class="focus:border-t-transparent focus:ring-0 peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900" placeholder=" " /><label class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">Cor
           </label>
@@ -54,7 +54,7 @@
       </div>
       <div class="w-72 mr-5"></div>
       <button type="submit" value="Salvar" onClick="Redirect()" class="ml-5 focus:outline-none text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-green-600 hover:bg-green-700 focus:ring-green-800">Cadastrar</button>
-      <button type="reset" value="Limpar" class="ml-24 focus:outline-none text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-red-600 hover:bg-red-700 focus:ring-red-900">Limpar</button>
+      <button type="reset" value="Remover" class="ml-24 focus:outline-none text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-red-600 hover:bg-red-700 focus:ring-red-900">Remover</button>
     </div>
   </div>
 </form>
